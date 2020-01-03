d4869ade0c10d7919140d0a74e7b0b8fe73e69d1
Alexander Szeto <alex.szeto.a@gmail.com>
2020-01-03 02:45:30 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,10 +5,10 @@ def reverse_each_word(string)
   word_split.each{ |word|
   reversed_word = []
     word.each{ |letter|
-      reversed_word
+      reversed_word.unshift(letter)
     }
-    
+    output_array << reversed_word.join()
   }
-  
+  return output_array
   
 end
